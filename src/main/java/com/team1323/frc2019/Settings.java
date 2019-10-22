4576bc42eb1b1271f06ed3a4542221461f42e9cc
MadTown Code Laptop 2 <madtownrobotics@maderausd.org>
2019-10-22 16:02:57 -0700

    HMMMM...

--- src/main/java/com/team1323/frc2019/Settings.java
+++ src/main/java/com/team1323/frc2019/Settings.java
@@ -12,7 +12,7 @@ package com.team1323.frc2019;
  */
 public class Settings {
 
-    public static final boolean kIsUsingCompBot = false;
+    public static final boolean kIsUsingCompBot = true;
 	public static final boolean kIsUsingTractionWheels = true;
 
 	public static final boolean kResetTalons = false;
@@ -24,7 +24,7 @@ public class Settings {
     public static final boolean kDebugSwerve = false;
     public static final boolean kDebugElevator = false;
     public static final boolean kDebugIntakes = false;
-    public static final boolean kDebugWrist = true;
+    public static final boolean kDebugWrist = false;
     public static final boolean kDebugJacks = false;
     public static final boolean kDebugVision = false;
 
